a9008a07190b465cf4c0bae978fa38d59f3bd837
Andy Stitt <andy@andystitt.com>
2016-07-19 18:07:47 -0400

    add clean css minifier to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 gulp.task('sass', function(){
   return gulp.src('scss/style.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
@@ -7,4 +8,9 @@ gulp.task('sass', function(){
 });
 gulp.task('watch', function () {
   gulp.watch('scss/style.scss', ['sass']);
+});
+gulp.task('minify-css', function() {
+  return gulp.src('style.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('production'));
 });
